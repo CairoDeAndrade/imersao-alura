a09aacb3825df09da428339ced17de41ffba5a83
unknown <cairo.andrade777@gmail.com>
2023-03-28 17:07:17 -0300

    Better treatment for showing data

--- consulming-api/src/Main.java
+++ consulming-api/src/Main.java
@@ -6,14 +6,16 @@ import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
+
+import static java.lang.Double.valueOf;
+import static java.lang.Math.round;
 
 
 public class Main {
     public static void main(String[] args) {
 
         // Make an HTTP connection consuming the API
-        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularMovies.json";
         URI uri = URI.create(url);
 
         HttpClient client = HttpClient.newHttpClient();
@@ -33,9 +35,18 @@ public class Main {
 
         // Display and manipulate the data
         for(Map<String, String> movie: moviesList){
-            System.out.println(movie.get("title"));
-            System.out.println(movie.get("image"));
-            System.out.println(movie.get("imDbRating"));
+            System.out.println("\u001b[1mTitle: " + movie.get("title") + "\u001b[m");
+            System.out.println("Image url:" + movie.get("image"));
+            System.out.println("Rating: \u001b[3m" + movie.get("imDbRating") + "\u001b[m");
+
+            int maxRate = 10;
+            int imDbRating = (int) round(valueOf(movie.get("imDbRating")));
+            for (int i = 0; i < maxRate; i++){
+                if (imDbRating == i) {
+                    String star = "⭐";
+                    System.out.println(star.repeat(i));
+                }
+            }
             System.out.println();
         }
     }
