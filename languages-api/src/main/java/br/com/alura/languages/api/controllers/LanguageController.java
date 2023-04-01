212713c928d271d6c47bcf5da89b776985998b17
Cairo Augusto de Andrade <cairo.andrade777@gmail.com>
2023-04-01 15:51:14 -0300

    Feat: Delete Language

--- languages-api/src/main/java/br/com/alura/languages/api/controllers/LanguageController.java
+++ languages-api/src/main/java/br/com/alura/languages/api/controllers/LanguageController.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,5 +38,11 @@ public class LanguageController {
 				.buildAndExpand(languageDTO.getId()).toUri();
 		
 		return ResponseEntity.created(uri).body(languageDTO);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
--- languages-api/src/main/java/br/com/alura/languages/api/services/LanguageService.java
+++ languages-api/src/main/java/br/com/alura/languages/api/services/LanguageService.java
@@ -4,12 +4,16 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.com.alura.languages.api.dto.LanguageDTO;
 import br.com.alura.languages.api.entities.Language;
 import br.com.alura.languages.api.repositories.LanguageRepository;
+import br.com.alura.languages.api.services.exceptions.DatabaseException;
+import br.com.alura.languages.api.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class LanguageService {
@@ -27,5 +31,17 @@ public class LanguageService {
 	public LanguageDTO insert(Language language) {
 		Language entity = repository.save(language);
 		return new LanguageDTO(entity);
+	}
+	
+	public void delete(String id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
 	}
 }
