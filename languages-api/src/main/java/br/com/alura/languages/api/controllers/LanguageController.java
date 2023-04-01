c80fc6e279b3dcedff0fa15ee569e69389680259
Cairo Augusto de Andrade <cairo.andrade777@gmail.com>
2023-04-01 15:42:37 -0300

    Fix: 201 created in post method and Exception Handling

--- languages-api/src/main/java/br/com/alura/languages/api/controllers/LanguageController.java
+++ languages-api/src/main/java/br/com/alura/languages/api/controllers/LanguageController.java
@@ -1,5 +1,6 @@
 package br.com.alura.languages.api.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,27 +8,33 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.alura.languages.api.dto.LanguageDTO;
 import br.com.alura.languages.api.entities.Language;
 import br.com.alura.languages.api.services.LanguageService;
 
 @RestController
+@RequestMapping(value = "/languages")
 public class LanguageController {
 	
 	@Autowired
 	private LanguageService service;
 	
-	@GetMapping("/languages")
+	@GetMapping
 	public ResponseEntity<List<LanguageDTO>> findAll() {
 		List<LanguageDTO> list = service.findAll();
 		return ResponseEntity.ok(list);
 	}
 	
-	@PostMapping("/languages")
+	@PostMapping
 	public ResponseEntity<LanguageDTO> insert(@RequestBody Language language){
 		LanguageDTO languageDTO = service.insert(language);
-		return ResponseEntity.ok().body(languageDTO);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(languageDTO.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(languageDTO);
 	}
 }
